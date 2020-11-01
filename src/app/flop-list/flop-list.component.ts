e33cab3ae915d0a525b3cb186a8e1efca0ee8f0a
Daniel-bin <daniel.coughlan@outlook.ie>
2020-11-01 19:00:01 +0000

    Finished!

--- src/app/flop-list/flop-list.component.ts
+++ src/app/flop-list/flop-list.component.ts
@@ -1,46 +1,40 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { FlopService } from './flop.service';
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { FlopService } from "./flop.service";
 import { Router } from "@angular/router";
 
 @Component({
-  selector: 'my-flop-list',
-  templateUrl: './flop-list.component.html',
-  styleUrls: [ './flop-list.component.css'
-  ]
+  selector: "my-flop-list",
+  templateUrl: "./flop-list.component.html",
+  styleUrls: ["./flop-list.component.css"]
 })
-export class FlopListComponent  {
+export class FlopListComponent {
   flops$;
-  rentalList: {count:number, titles:string};
+  rentalList: { count: number; titles: string };
 
-  constructor(private flopService: FlopService, private router: Router) {
-
-  }
+  constructor(private flopService: FlopService, private router: Router) {}
 
   ngOnInit() {
-    this.flops$ = this.flopService.getFlops()
-    
+    this.flops$ = this.flopService.getFlops();
+
     this.getRentalList();
   }
 
   getRentalList() {
-    this.flopService.getRentalList()
-    .subscribe(rentalList => {
-      
+    this.flopService.getRentalList().subscribe(rentalList => {
       this.rentalList = rentalList;
 
-      if(this.rentalList.count) {}
-      
-    })
+      if (this.rentalList.count) {
+      }
+    });
   }
 
-
   onRentClick(event, flop) {
-    console.log(event)
+    console.log(event);
     event.stopPropagation();
-    event.preventDefault()
+    event.preventDefault();
     this.flopService.addRental(flop.title);
     this.getRentalList();
-  
+
     console.log("length:" + this.rentalList.count);
     if (this.rentalList.count > 10) {
       this.router.navigate(["/toomany"]);
